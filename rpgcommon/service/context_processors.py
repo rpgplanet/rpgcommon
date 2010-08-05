616428f6e346b1b3fefad1d591ae3e9355af0982
Almad <bugs@almad.net>
2010-08-05 14:07:37 +0200

    Inject GA code

--- rpgcommon/service/context_processors.py
+++ rpgcommon/service/context_processors.py
@@ -2,5 +2,6 @@ from django.conf import settings
 
 def service_tokens(request):
     return {
-        'facebook_application_id': getattr(settings, 'FACEBOOK_APPLICATION_ID', '')
+        'facebook_application_id': getattr(settings, 'FACEBOOK_APPLICATION_ID', ''),
+        'google_analytics_code' : getattr(settings, 'GOOGLE_ANALYTICS_CODE', ''),
     }
