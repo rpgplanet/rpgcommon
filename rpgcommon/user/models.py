f96a9cd40246032fc5a127fccf350ff80f6789e6
Almad <bugs@almad.net>
2010-08-08 15:05:28 +0200

    User can register with FB account (refs #16)

--- rpgcommon/user/models.py
+++ rpgcommon/user/models.py
@@ -1,11 +1,13 @@
 from django.db.models import (
     Model,
-    CharField,
+    CharField, TextField, DateTimeField,
     ForeignKey,
 )
 
 from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
+
+from anyjson import serialize, deserialize
 
 class UserProfile(Model):
     user = ForeignKey(User, unique=True)
@@ -14,3 +16,20 @@ class UserProfile(Model):
 
 class InvitedEmail(Model):
     email = CharField(max_length=50, unique=True)
+
+class FacebookAccount(Model):
+    user = ForeignKey(User, unique=True)
+    facebook_uid = CharField(max_length=30, unique=True)
+    access_token = CharField(max_length=255, unique=True)
+    expires = DateTimeField()
+    secret = CharField(max_length=50, unique=True)
+    signature = CharField(max_length=50)
+    session_key = CharField(max_length=255)
+    data = TextField()
+
+    def get_json_data(self):
+        return deserialize(self.data)
+
+    def set_json_data(self, data):
+        self.data = serialize(data)
+        return self.data
--- rpgcommon/user/views.py
+++ rpgcommon/user/views.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
+import warnings
+
+from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
@@ -12,8 +16,11 @@ from django.forms import (
 
 from django.views.generic.simple import direct_to_template
 
-from rpgcommon.user.models import InvitedEmail
+import facebook
+
+from rpgcommon.user.models import InvitedEmail, FacebookAccount
 from rpgcommon.user.user import create_user
+
 
 class RegistrationForm(Form):
     username = CharField(label="Uživatelské jméno", max_length=30)
@@ -49,7 +56,12 @@ class RegistrationForm(Form):
         return self.cleaned_data.get("username", "")
 
 
+class FacebookForm(Form):
+    username = CharField(label="Uživatelské jméno", max_length=30)
+
 def inviteform(request, template='registration/inviteform.html'):
+    message = ''
+    
     if request.POST.get("traditional_registration", None):
         registration_form = RegistrationForm(request.POST)
         if registration_form.is_valid():
@@ -64,7 +76,71 @@ def inviteform(request, template='registration/inviteform.html'):
             }))
     else:
         registration_form = RegistrationForm()
-    
+
+    if not getattr(settings, "FACEBOOK_APPLICATION_ID", None) or not getattr(settings, "FACEBOOK_APPLICATION_SECRET", None):
+        warnings.warn("FACEBOOK_APPLICATION_ID or FACEBOOK_APPLICATION_SECRET not given, not handling FB authentication")
+
+    if request.POST.get("facebook_registration", None):
+        fb_form = FacebookForm(request.POST)
+        fb_form.visible = True
+
+        if fb_form.is_valid():
+            if not getattr(settings, "FACEBOOK_APPLICATION_ID", None) or not getattr(settings, "FACEBOOK_APPLICATION_SECRET", None):
+                message = u"Aplikace bohužel nemá k dispozici nastavení potřebná na používání s Facebookem :-/"
+            else:
+                fb_info = facebook.get_user_from_cookie(request.COOKIES, settings.FACEBOOK_APPLICATION_ID, settings.FACEBOOK_APPLICATION_SECRET)
+
+                if not fb_info:
+                    message = u"Nepodařilo se získat informace z Facebooku. Má problémy? Nebo došlo k chybě při přenosu informací? Zkuste to znovu."
+                else:
+                    graph = facebook.GraphAPI(fb_info["access_token"])
+                    profile = graph.get_object("me")
+        #            friends = graph.get_connections("me", "friends")
+
+                    if 'email' not in profile:
+                        message = u"Neposkytl jsi mi e-mail, ale já ho skutečně potřebuji pro fungování. Prosím odhlaš se z Facebook a tentokrát mi ho poskytni :o)"
+                    else:
+                        try:
+                            InvitedEmail.objects.get(email=profile['email'])
+                        except InvitedEmail.DoesNotExist, err:
+                            message = u"Na tento e-mail není dostupná žádná pozvánka. Omlouváme se - poprostě někoho registrovaného, nebo se přijďte podívat později!"
+                        else:
+                            # finally, we can create profile
+                            user = create_user(
+                                username = fb_form.cleaned_data['username'],
+                                password = '',
+                                email = profile['email']
+                            )
+
+                            account = FacebookAccount(
+                                user = user
+                            )
+
+                            for fb_key, model_key in (
+                                ("uid", "facebook_uid"),
+                                ("access_token", "access_token"),
+                                ("secret", "secret"),
+                                ("sig", "signature"),
+                                ("session_key", "session_key"),
+                            ):
+                                setattr(account, model_key, fb_info[fb_key])
+
+                            account.expires = datetime.utcfromtimestamp(float(fb_info['expires']))
+
+                            account.set_json_data(profile)
+
+                            account.save()
+
+                            return HttpResponseRedirect(reverse('service:profile', kwargs={
+                                "user_slug" : user.get_profile().slug
+                            }))
+
+    else:
+        fb_form = FacebookForm()
+        fb_form.visible = False
+
     return direct_to_template(request, template, {
         'registration_form' : registration_form,
+        'fb_form' : fb_form,
+        'message' : message,
     })
