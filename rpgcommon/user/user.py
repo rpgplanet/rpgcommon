5f17ee390608e8651282a4440691c4181cb4f935
Almad <bugs@almad.net>
2010-08-28 20:52:07 +0200

    Working logout (with FB) (refs #34)

--- rpgcommon/user/user.py
+++ rpgcommon/user/user.py
@@ -1,10 +1,14 @@
+
+from copy import copy
+from hashlib import md5
+from time import time
+from urllib import urlencode
+
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
 
 import facebook
-
-from time import time
 
 from rpgcommon.user.models import UserProfile
 from rpgcommon.service.utils import unixize_name
@@ -39,46 +43,29 @@ def logout(request):
     django_logout(request)
 
 
-###########
-### set_fb_cookie and cookie_signature taken from Facebook Python SDK examples
-### Copyright 2010 Facebook
-### Licensed under the Apache License, Version 2.0 (the "License")
-### See http://github.com/facebook/python-sdk/blob/master/examples/oauth/facebookoauth.py
-### Modified for Django usage by Almad
-##########
-def cookie_signature(*parts):
-    """Generates a cookie signature.
-
-    We use the Facebook app secret since it is different for every app (so
-    people using this example don't accidentally all use the same secret).
-    """
-    import hashlib
-    import hmac
-    hash = hmac.new(settings.FACEBOOK_APPLICATION_SECRET, digestmod=hashlib.sha1)
-    for part in parts: hash.update(part)
-    return hash.hexdigest()
-
-def set_fb_cookie(response, name, value, domain=None, path="/", expires=None):
-    """Generates and signs a cookie for the give name/value"""
-    import base64
-    import Cookie
-    import email.utils
-    
-    timestamp = str(int(time()))
-    value = base64.b64encode(value)
-    signature = cookie_signature(value, timestamp)
-    cookie = Cookie.BaseCookie()
-    cookie[name] = "|".join([value, timestamp, signature])
-    cookie[name]["path"] = path
-    if domain: cookie[name]["domain"] = domain
-    if expires:
-        cookie[name]["expires"] = email.utils.formatdate(
-            expires, localtime=False, usegmt=True)
-    response["Set-Cookie"] = cookie.output()[12:]
-
 def fb_logout(cookies, response):
     fb_info = facebook.get_user_from_cookie(cookies, settings.FACEBOOK_APPLICATION_ID, settings.FACEBOOK_APPLICATION_SECRET)
     if fb_info:
-        set_fb_cookie(response, "fb_user", "", expires=time() - 86400)
+        fb_info['uid'] = ""
+        fb_info['access_token'] = ""
 
+        ###########
+        ### just reverse facebook.get_user_from_cookie.
+        ### Original get_user_from_cookie is from Facebook Python SDK
+        ### Copyright 2010 Facebook
+        ### Licensed under the Apache License, Version 2.0 (the "License")
+        ### Modified for set by Almad
+        ##########
+
+        response['P3P'] = 'CP="NOI DSP COR NID ADMa OPTa OUR NOR"'
+        
+        expire_time = time() - 86400
+
+        args = copy(fb_info)
+        args['expires'] = str(int(expire_time))
+        payload = "".join([k + "=" + fb_info[k] for k in sorted(args.keys()) if k != "sig"])
+        
+        args['sig'] = md5(payload+settings.FACEBOOK_APPLICATION_SECRET).hexdigest()
+
+        response.set_cookie('fbs_%s' % settings.FACEBOOK_APPLICATION_ID , urlencode(args), expires=expire_time)
 
